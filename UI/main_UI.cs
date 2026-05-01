d295b667a115ad732c296c134e72aa6e7501fe0d
Cesar Guanipa <cesar10guanipa@gmail.com>
2026-05-01 00:19:29 -0300

    feat: esqueleto general del proyecto

---
+++ UI/main_UI.cs
@@ -0,0 +1,15 @@
+namespace UI
+{
+    public partial class main_UI : Form
+    {
+        public main_UI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ servicios/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace servicios
+{
+    public class Class1
+    {
+
+    }
+}
