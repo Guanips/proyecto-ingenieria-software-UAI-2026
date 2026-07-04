db78f9145b1505552f29f6fc0d89c6ef4ad59d7f
Cesar Guanipa <cesar10guanipa@gmail.com>
2026-07-04 08:56:00 -0300

    Comment out user backup and deletion logic
    Commented out code for backup and deletion of users.

--- DAL/RepositorioIntegridad.cs
+++ DAL/RepositorioIntegridad.cs
@@ -1,4 +1,4 @@
-﻿using BE;
+using BE;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -77,50 +77,50 @@ namespace DAL
 
         public void GuardarBackup(Usuario u)
         {
-            DataSet ds = DAO.GetInstance.ObtenerDataSet();
-            DataTable? dtBackup = ds.Tables["UsuarioBackup"];
+            //DataSet ds = DAO.GetInstance.ObtenerDataSet();
+            //DataTable? dtBackup = ds.Tables["UsuarioBackup"];
 
-            if (dtBackup == null) throw new Exception("La tabla UsuarioBackup no existe en el DataSet.");
+            //if (dtBackup == null) throw new Exception("La tabla UsuarioBackup no existe en el DataSet.");
 
-            DataRow[] rows = dtBackup.Select($"ID = '{u.Id}'");
-            DataRow row;
+            //DataRow[] rows = dtBackup.Select($"ID = '{u.Id}'");
+            //DataRow row;
 
-            if (rows.Length > 0)
-            {
-                row = rows[0];
-            }
-            else
-            {
-                row = dtBackup.NewRow();
-                row["ID"] = u.Id;
-                dtBackup.Rows.Add(row);
-            }
+            //if (rows.Length > 0)
+            //{
+            //    row = rows[0];
+            //}
+            //else
+            //{
+            //    row = dtBackup.NewRow();
+            //    row["ID"] = u.Id;
+            //    dtBackup.Rows.Add(row);
+            //}
 
-            row["Username"] = u.Username;
-            row["PasswordHash"] = u.PasswordHash;
-            row["Email"] = u.Email;
-            row["NumTelefono"] = u.NumTelefono;
-            row["EstaBloqueado"] = u.EstaBloqueado;
-            row["Idioma"] = u.Idioma;
-            row["IntentosFallidos"] = u.IntentosFallidos;
-            row["DVH"] = string.IsNullOrEmpty(u.DVH) ? (object)DBNull.Value : u.DVH;
+            //row["Username"] = u.Username;
+            //row["PasswordHash"] = u.PasswordHash;
+            //row["Email"] = u.Email;
+            //row["NumTelefono"] = u.NumTelefono;
+            //row["EstaBloqueado"] = u.EstaBloqueado;
+            //row["Idioma"] = u.Idioma;
+            //row["IntentosFallidos"] = u.IntentosFallidos;
+            //row["DVH"] = string.IsNullOrEmpty(u.DVH) ? (object)DBNull.Value : u.DVH;
 
-            DAO.GetInstance.SubirCambiosBD();
+            //DAO.GetInstance.SubirCambiosBD();
         }
 
         public void EliminarDeBackup(Guid idUsuario)
         {
-            DataSet ds = DAO.GetInstance.ObtenerDataSet();
-            DataTable? dtBackup = ds.Tables["UsuarioBackup"];
+            //DataSet ds = DAO.GetInstance.ObtenerDataSet();
+            //DataTable? dtBackup = ds.Tables["UsuarioBackup"];
 
-            if (dtBackup == null) throw new Exception("La tabla UsuarioBackup no existe en el DataSet.");
+            //if (dtBackup == null) throw new Exception("La tabla UsuarioBackup no existe en el DataSet.");
 
-            DataRow[] rows = dtBackup.Select($"ID = '{idUsuario}'");
-            if (rows.Length > 0)
-            {
-                rows[0].Delete();
-                DAO.GetInstance.SubirCambiosBD();
-            }
+            //DataRow[] rows = dtBackup.Select($"ID = '{idUsuario}'");
+            //if (rows.Length > 0)
+            //{
+            //    rows[0].Delete();
+            //    DAO.GetInstance.SubirCambiosBD();
+            //}
         }
     }
 }
